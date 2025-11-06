0891e3fa07551cf195de1eeeea46413b7c13859b
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-11-06 14:24:22 +0600

    main layout setup completed

---
+++ src/app/(Main-Public)/layout.js
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }) {
+  return (
+    <section>
+      <header></header>
+      <main>{children}</main>
+      <footer></footer>
+    </section>
+  );
+}
