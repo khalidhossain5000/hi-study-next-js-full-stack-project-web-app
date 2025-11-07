41e786e1433b81b3cd771735f0f73e6be1316d75
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-11-07 19:02:32 +0600

    world map with country logo orbit working added

--- src/components/HomePage/BannerSection/Banner.jsx
+++ src/components/HomePage/BannerSection/Banner.jsx
@@ -1,17 +1,27 @@
 import ButtonIcon from "@/components/Shared/Button/ButtonIcon";
 import React from "react";
+import FlagOrbit from "./FlagOrbit";
 
 const Banner = () => {
   return (
-    <div className="">
-      <div className="content text-center lg:text-left space-y-6">
-       <h5 className="text-sm lg:text-[18px] bg-linear-to-t from-[#394ef4] to-[#b966e7] bg-clip-text text-transparent">
-  Multi-Language Academy
-</h5>
+    <div className="flex flex-col-reverse lg:flex-row items-center ">
+      <div className="flex-1 content text-center lg:text-left space-y-6 px-2">
+        <h5 className="text-sm lg:text-[18px] bg-linear-to-t from-[#394ef4] to-[#b966e7] bg-clip-text text-transparent">
+          Multi-Language Academy
+        </h5>
 
-        <h2 className="text-2xl md:text-3xl xl:text-[60px] text-[#192335] font-bold max-w-xl font-gabriela">Take Online Language Courses With Certificate</h2>
-        <p className="text-xl text-gray-900 max-w-[560px]">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat.</p>
+        <h2 className="text-2xl md:text-3xl xl:text-[60px] text-[#192335] font-bold font-gabriela">
+          Take Online Language And Technical Courses With Certificate
+        </h2>
+        <p className="text-xl text-gray-900 max-w-[560px]">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat.
+        </p>
         <ButtonIcon>Find Your Courses</ButtonIcon>
+      </div>
+      {/* flag orbit component here */}
+      <div className="relative flex items-center justify-center  flex-1">
+        <FlagOrbit />
       </div>
     </div>
   );
--- src/components/HomePage/BannerSection/FlagOrbit.jsx
+++ src/components/HomePage/BannerSection/FlagOrbit.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
-
+"use client";
+import Image from "next/image";
+import React from "react";
+import worldmap from "../../../assets/banner/world.png";
+import australia from "../../../assets/banner/flag-and-icon-r/flag-australia.png";
+import canada from "../../../assets/banner/flag-and-icon-r/flag-canada.png";
+import china from "../../../assets/banner/flag-and-icon-r/flag-china.png";
+import france from "../../../assets/banner/flag-and-icon-r/flag-french.png";
+import germany from "../../../assets/banner/flag-and-icon-r/flag-germany.png";
+import japan from "../../../assets/banner/flag-and-icon-r/flag-japan.png";
+import korea from "../../../assets/banner/flag-and-icon-r/flag-korea.png";
+import pakistan from "../../../assets/banner/flag-and-icon-r/flag-pakistan.png";
+import srilanka from "../../../assets/banner/flag-and-icon-r/flag-srilonka.png";
+import uk from "../../../assets/banner/flag-and-icon-r/flag-uk.png";
+import usa from "../../../assets/banner/flag-and-icon-r/flag-usa.png";
+import { motion } from "framer-motion";
 const FlagOrbit = () => {
-    return (
-        <div>
-            
+  return (
+    <div className=" relative flex items-center justify-center w-[500px] h-[500px]  mx-auto ">
+      {/* flags world map container here */}
+      <div className="relative">
+        <Image
+          src={worldmap}
+          alt="world map"
+          width={500}
+          height={500}
+          className="w-full relative z-10"
+        />
+      </div>
+      {/* all flags container */}
+      <motion.div
+       
+        className="all-flags flex justify-center rounded-full w-[500px] h-[500px] absolute top-0 border-2 border-dashed border-gray-600 z-10"
+      >
+        {/* aus */}
+        <div className="absolute z-10 w-[60px] h-[60px] rounded-full flex items-center justify-center -top-9">
+          <Image
+            src={australia}
+            alt="australia"
+            width={60}
+            height={60}
+            className=""
+          />
         </div>
-    );
+        <div className=" ">
+          <Image src={canada} alt="australia" width={60} height={60} />
+        </div>
+        <div className=" ">
+          <Image src={china} alt="australia" width={60} height={60} />
+        </div>
+        {/* franse */}
+        <div className="">
+          <Image src={france} alt="australia" width={60} height={60} />
+        </div>
+        {/* germany */}
+        <div className="">
+          <Image src={germany} alt="australia" width={60} height={60} />
+        </div>
+        {/* japans */}
+        <div className=" ">
+          <Image src={japan} alt="australia" width={60} height={60} />
+        </div>
+        {/* austranlia */}
+        <div className="  ">
+          <Image src={korea} alt="australia" width={60} height={60} />
+        </div>
+        {/* pakistan */}
+        <div className="  ">
+          <Image src={pakistan} alt="australia" width={60} height={60} />
+        </div>
+        {/* lanka */}
+        <div className=" ">
+          <Image src={srilanka} alt="australia" width={60} height={60} />
+        </div>
+        {/* uk */}
+        <div className=" ">
+          <Image src={uk} alt="australia" width={60} height={60} />
+        </div>
+        {/* usa */}
+        <div className=" ">
+          <Image src={usa} alt="australia" width={60} height={60} />
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default FlagOrbit;
