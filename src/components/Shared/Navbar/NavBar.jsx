234a4bcddee8ec8cb85efbc59a3f1f80035c077a
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-11-08 22:01:01 +0600

    cmntst added

--- src/components/Shared/Navbar/NavBar.jsx
+++ src/components/Shared/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import lightLogo from "../../../assets/logo/logo-black.png";
 import darklogo from "../../../assets/logo/logo-light.png";
@@ -15,9 +15,9 @@ import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/ui/tooltip";
 const NavBar = () => {
-  const{data:session}=useSession()
+  const { data: session } = useSession();
   console.log(session);
   return (
     <div className="bg-[#ffffff] dark:bg-[#273041] py-3 2xl:py-3 shadow-md px-2 ">
@@ -40,56 +40,56 @@ const NavBar = () => {
         <div className="auth-btns">
           <div className="hidden lg:flex items-center gap-6">
             <div>
-
-            <ModeToggle/>
+              <ModeToggle />
             </div>
             <div className="hidden lg:flex items-center gap-6">
               <div>
-                
-              
-
-
-    <Tooltip>
-      <TooltipTrigger asChild>
-       <Image
-                src={session?.user?.image || 'https://i.ibb.co/zVB99J4d/DEFAULT.jpg'}
-                alt="user profile image with avatar here"
-                width={50}
-                height={50}
-                className="rounded-full border-2 border-indigo-600 p-1 cursor-pointer"
-                />
-      </TooltipTrigger>
-      <TooltipContent className={`bg-[#d176da] p-2 text-md`}>
-        <p>{session?.user?.name}</p>
-      </TooltipContent>
-    </Tooltip>
-
+                {/* user name showing on tooltip image hover */}
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Image
+                      src={
+                        session?.user?.image ||
+                        "https://i.ibb.co/zVB99J4d/DEFAULT.jpg"
+                      }
+                      alt="user profile image with avatar here"
+                      width={50}
+                      height={50}
+                      className="rounded-full border-2 border-indigo-600 p-1 cursor-pointer"
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent className={`bg-[#d176da] p-2 text-md`}>
+                    <p>{session?.user?.name}</p>
+                  </TooltipContent>
+                </Tooltip>
               </div>
               <div>
-                {
-              !session?.user? <Link href={`/auth/login`}><Button>Login</Button></Link> : <AnimatedButton
-            className="bg-linear-to-r from-[#f70449] to-[#fb2f01] hover:scale-105 lg:p-6 text-white w-full css"
-            variant="default"
-            size="default"
-            glow={false}
-            textEffect="glitch"
-            uppercase={true}
-            rounded="custom"
-            asChild={false}
-            hideAnimations={false}
-            shimmerColor="#f1f1f3"
-            shimmerSize="0.20em"
-            shimmerDuration="2s"
-            borderRadius="10px"
-            background="rgba(0, 0, 0, 1)"
-          >
-            Log Out
-          </AnimatedButton>
-            }
+                {!session?.user ? (
+                  <Link href={`/auth/login`}>
+                    <Button>Login</Button>
+                  </Link>
+                ) : (
+                  <AnimatedButton
+                    className="bg-linear-to-r from-[#f70449] to-[#fb2f01] hover:scale-105 lg:p-6 text-white w-full css"
+                    variant="default"
+                    size="default"
+                    glow={false}
+                    textEffect="glitch"
+                    uppercase={true}
+                    rounded="custom"
+                    asChild={false}
+                    hideAnimations={false}
+                    shimmerColor="#f1f1f3"
+                    shimmerSize="0.20em"
+                    shimmerDuration="2s"
+                    borderRadius="10px"
+                    background="rgba(0, 0, 0, 1)"
+                  >
+                    Log Out
+                  </AnimatedButton>
+                )}
               </div>
             </div>
-            
-            
           </div>
           <HomeResponsiveMenu />
         </div>
